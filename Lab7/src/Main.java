d0786f613f5780b521494c8089b85d81e4af624d
Thomas Ideapad <thomaspowisd@gmail.com>
2023-03-22 10:46:30 -0400

    Reading through the files

--- Lab7/src/Main.java
+++ Lab7/src/Main.java
@@ -1,11 +1,29 @@
-
+import java.io.File;
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		readSongs();
 	}
 	
+	//Method to read in the songs
+	public static void readSongs() {
+		String currentDirectory = System.getProperty("user.dir");
+		String directoryPath = currentDirectory + "\\src\\Queen";
+		File directory = new File(directoryPath);
+		File[] files = directory.listFiles();
+
+        // Loop through the files and print their names
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println(file.getName());
+                }
+            }
+        }
+    
+	}
 	
 
 }
