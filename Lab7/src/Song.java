208189f4303b33d7a53a74f3822f183a4b623dc1
Thomas Ideapad <thomaspowisd@gmail.com>
2023-03-22 10:22:47 -0400

    Making the song object

---
+++ Lab7/src/Song.java
@@ -0,0 +1,18 @@
+
+public class Song {
+	String title;
+	String lyrics;
+	public Song(String title, String lyrics) {
+		this.title = title;
+		this.lyrics = lyrics;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getLyrics() {
+		return lyrics;
+	}
+	public String toString() {
+		return "Title: " + getTitle() + " Lyrics: " + getLyrics();
+	}
+}
