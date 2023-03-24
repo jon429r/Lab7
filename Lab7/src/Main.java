2f46d8064f9bc0e92b16f8419db98a8ff318825a
jon429r <jonathanday088@gmail.com>
2023-03-23 21:08:29 -0400

    asks for input

--- Lab7/src/Main.java
+++ Lab7/src/Main.java
@@ -6,11 +6,12 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		ArrayList<Song> allSongs = readSongs();
-		
+
 		//A quick test line
 		System.out.println(allSongs.get(0));
+		input();
 	}
-	
+
 	//Method to read in the songs
 	public static ArrayList<Song> readSongs() throws FileNotFoundException {
 		String currentDirectory = System.getProperty("user.dir");
@@ -18,31 +19,35 @@ public class Main {
 		File directory = new File(directoryPath);
 		File[] files = directory.listFiles();
 
-        ArrayList<Song> allSongs = new ArrayList<Song>();
+		ArrayList<Song> allSongs = new ArrayList<Song>();
 		String line = "";
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile()) {
-                	String title = file.getName();
-                	int period = title.lastIndexOf(".txt");
-                	title = title.substring(0,period);
+		if (files != null) {
+			for (File file : files) {
+				if (file.isFile()) {
+					String title = file.getName();
+					int period = title.lastIndexOf(".txt");
+					title = title.substring(0,period);
 
-                    Scanner sc = new Scanner(new File(directoryPath + "/"+ file.getName()));
-                    while(sc.hasNextLine()){
-                    	line += sc.nextLine() + "\n";
-                    }
-                    Song newSong = new Song(title,line);
-                    allSongs.add(newSong);
-                }
-            }
-        }
-        return allSongs;
+					Scanner sc = new Scanner(new File(directoryPath + "/"+ file.getName()));
+					while(sc.hasNextLine()){
+						line += sc.nextLine() + "\n";
+					}
+					Song newSong = new Song(title,line);
+					allSongs.add(newSong);
+				}
+			}
+		}
+		return allSongs;
 	}
-	
+
 	public static String[] input() {
+		
+		System.out.println("What would you like to search for?");
+
 		Scanner scnr = new Scanner(System.in);
 		String query = scnr.next();
 		
+
 		String[] input = query.split("\\s+");
 		scnr.close();
 		return input;
